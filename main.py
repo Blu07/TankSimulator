e4387237ba6cca9647b53befa62b4fbcc0b5a0c7
Blu William Opland <bluw2007@gmail.com>
2024-12-05 14:55:12 +0100

    final

--- main.py
+++ main.py
@@ -41,7 +41,8 @@ class Tank:
 
     
     def stepSimulation(self, step):
-        pressureAtHole = self.presExtra + self.rho * 9.81 * (self.lqHeight - self.outH) # g = 9.81
+        pressureFromInQ = self.inQ / (np.pi * self.r**2)
+        pressureAtHole = self.presExtra + self.rho * 9.81 * (self.lqHeight - self.outH) + pressureFromInQ # g = 9.81
         
         if self.outTank:
             presDiffOut = pressureAtHole - self.outTank.presExtra
@@ -85,18 +86,16 @@ if __name__ == "__main__":
     rho = 1000
     liquidHeight = 3.7
     
-    extraPressure = 101.5e3
-    
+    extraPressure = 101.5e3 # Atmospheric pressure
     
 
-
+    # Get the number of Tanks from command line argument
     parser = argparse.ArgumentParser()
     parser.add_argument('numTanks', type=int, help='Number of tanks.')
     args = parser.parse_args()
     numTanks = args.numTanks
 
 
-    # Creates a list of all the tank objects from bottom tank to top tank.
     tankTemplate = Tank(
                 h=tankHeight,
                 r=tankRadius,
@@ -108,8 +107,9 @@ if __name__ == "__main__":
                 outTank=None,
             )
     
-    tanks: list[Tank] = [tankTemplate] # This is the last tank; the outTank is None.
-    valueList: list[list] = [[]]
+    # Create list of all the tank objects from bottom tank to top tank.
+    tanks: list[Tank] = [tankTemplate] # The first index is the last tank; the outTank is None.
+    valueList: list[list] = [[]] # Store a list for each tank, containing their volume records over time
     
     # Copy the template, set the outTank to the last tank in tanks (being the heighest one up)
     for _ in range(1, numTanks):
@@ -119,30 +119,37 @@ if __name__ == "__main__":
         tanks.append(newTank)
         valueList.append([])
     
+    
+    
     # Go through every tank for every time step.
     timeList = [] # running generation; the x-axis of plot
     time = 0
     
     # Put all the formulas in the simulation into one equation, solve for "step".
     # This makes sure that the volume change per step is not larger than some threshold, giving a nice resolution compared to compution time for any configuration and number of tanks.
-    stepMillis = min(np.sqrt(numTanks) / 100, 0.5) * ( (tankHeight - outHeight) * tankRadius**2) / (outRadius**2 * np.sqrt( (2 * 9.81 * (liquidHeight - outHeight))) )
-
-    lowestVolume = tanks[0].outH * tanks[0].r**2 * np.pi
+    k = min(np.sqrt(250*numTanks)/1000, 0.5) # k must be under 1, optimally around 0.01 - 0.1. Testing shows a value of 0.5 gives a bad, but still acceptable, error.
+    stepMillis = min(k, 0.5) * ( (tankHeight - outHeight) * tankRadius**2) / (outRadius**2 * np.sqrt( (2 * 9.81 * (liquidHeight - outHeight))) )
+    print(f'k = {k}')
     
+    
+    # Record the time the simulation takes to compute
     start = perf_counter()
 
+    lowestVolume = np.pi * tankRadius**2 * outHeight
     while tanks[0].volume > lowestVolume:
         timeList.append(time)
-        # print(f"Simulation time: {time/1000} sec")
+        print(f"Simulation time: {time:.2f} sec")
         for i, tank in enumerate(tanks):
             tank.stepSimulation(stepMillis)
             valueList[i].append(tank.volume)
             
-        time += stepMillis/1000 # Store seconds instead of milliseconds
-    end = perf_counter()
+        time += stepMillis/1000 # Store as seconds instead of milliseconds
     
+    end = perf_counter()
     elapsed = end - start
-    print(f'Time taken: {elapsed:.6f} seconds')
+    print(f'Time spent: {elapsed:.6f} seconds')
+    
+    
     
     # Plot every graph with the same x-axis with time
     for y in valueList:
@@ -156,21 +163,3 @@ if __name__ == "__main__":
         
     
 
-
-
-# Testing shows this performance:
-    # 10000 tanks:
-    #   error: 3.2L
-    #   time: 507 seconds
-    # 1000 tanks:
-    #   error = 1.2L,
-    #   time = 6.75 seconds
-    # 100 tanks:
-    #   error: 0.15L
-    #   time: 0.18 seconds
-    # 10 tanks:
-    #   error = 0.011L,
-    #   time = 0.0064 seconds
-    # 1 tank:
-    #   error: ~0L
-    #   time: 0.00036 seconds
